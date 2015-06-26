53ad619c5d18d0833d6b209dc04339a5b527f1a9
Kotaro Hara <koe.bluebear@gmail.com>
2015-06-26 02:21:33 -0400

    Export geojson

--- build.sbt
+++ build.sbt
@@ -1,16 +1,17 @@
-name := """jts-tutorial"""
+name := """jts-scala-tutorial"""
 
 version := "1.0"
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.10.4"
 
-// Change this to another test framework if you prefer
+resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+
 libraryDependencies ++= Seq(
+  "com.typesafe.play" %% "play-json" % "2.3.4",
+  "com.typesafe.play.extras" %% "play-geojson" % "1.2.0",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test",
   "com.vividsolutions" % "jts" % "1.13"
 )
 
-
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
-
--- src/main/scala/com/example/Example02.scala
+++ src/main/scala/com/example/Example02.scala
@@ -1,7 +1,10 @@
 package com.example
 
-import com.vividsolutions.jts.geom.{Geometry, Point}
+import com.vividsolutions.jts.geom
 import com.vividsolutions.jts.io.{WKTWriter, WKTReader, WKBWriter, WKBReader}
+
+import play.api.libs.json._
+import play.extras.geojson._
 
 
 object Example02 {
@@ -9,13 +12,16 @@ object Example02 {
   // case class GeometryBean(id: Long, geom: Geometry)
   val wktReader = new WKTReader()
   val wktWriter = new WKTWriter()
-  val wkbWriter = new WKBWriter()
-  val wkbReader = new WKBReader()
 
   def main(args: Array[String]): Unit = {
-    val POINT = "POINT(-71.064544 42.28787)"
-    val point = wktReader.read(POINT).asInstanceOf[Point]
-    println("hi")
-    println(point.toString)
+    val POINT = "POINT(151.2111 -33.86)"
+    val point = wktReader.read(POINT).asInstanceOf[geom.Point]
+    val coord = point.getCoordinate
+    println(point)
+
+    val sydney = Feature(Point(LatLng(coord.y, coord.x)), properties=Some(Json.obj("name" -> "Sydney")))
+    val json = Json.toJson(sydney)
+
+    println(json)
   }
 }
